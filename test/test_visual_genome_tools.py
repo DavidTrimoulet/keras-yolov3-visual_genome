97bcf5d3decea85672870e87362d791817260755
David Trimoulet <davidt@hotmail.fr>
2019-06-18 16:58:24 +0200

    [UPDATE] Splittingdataset cleaning and pairs creation for specific algorithms

--- test/test_visual_genome_tools.py
+++ test/test_visual_genome_tools.py
@@ -8,14 +8,27 @@ class TestVisualGenomeTools(TestCase):
     def test_convert_object_yolo(self):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
-        visual_genome_tools.convert_object_for_yolo_v3( p.absolute(), "objects.json" )
+        vgt = visual_genome_tools.VisualGenomeTools(path_to_visual_genome_folder=p)
+        vgt.convert_object_for_yolo_v3("objects.json" )
 
     def test_convert_object_retina(self):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
-        visual_genome_tools.convert_object_for_retina( p.absolute(), "objects.json" )
+        vgt = visual_genome_tools.VisualGenomeTools(path_to_visual_genome_folder=p)
+        visual_genome_tools.convert_object_for_retina(  "objects.json" )
 
     def test_image_converter_sanity_check(self):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
-        visual_genome_tools.display_image_bb(p, "yolo_objects", "yolo_object_to_id")
+        vgt = visual_genome_tools.VisualGenomeTools(p)
+        visual_genome_tools.display_image_bb("yolo_objects", "yolo_object_to_id")
+
+    def test_load_glove(self):
+        p = Path('.')
+        p = p / ".." / ".." / "Visual_Genome"
+        vgt = visual_genome_tools.VisualGenomeTools(p, load_glove=True)
+
+    def test_clean_visual_genome_data(self:
+        p = Path('.')
+        p = p / ".." / ".." / "Visual_Genome"
+        vgt = visual_genome_tools.clean_visual_genome_data(filename="objects.json")
--- tools/visual_genome_tools.py
+++ tools/visual_genome_tools.py
@@ -3,113 +3,177 @@
 from pathlib import Path
 import json
 import cv2
+import re
 
 
-def convert_object_for_yolo_v3(path= Path('.'), filename="objects.json"):
-    densecap_file = path / filename
-    yolo_file = path / "yolo_objects"
-    id_to_object_file = path / "yolo_object_to_id"
-    object_to_id = {}
-    object_id=0
-    with open(str(densecap_file), 'r') as df:
-        with open(str(yolo_file), 'w+') as yf:
-            data = json.load(df)
-            image_count = 0
+class VisualGenomeTools:
+
+    def __init__(self, load_glove=False, path_to_visual_genome_folder=Path('.')):
+        self.path = path_to_visual_genome_folder
+        if load_glove:
+            self.vocab = self.load_glove()
+
+    def get_vocab(self):
+        if not self.vocab :
+            self.load_glove()
+        return self.vocab
+
+    def clean_visual_genome_data(self, filename="objects.json"):
+        vg_file = self.path / filename
+        vg_clean_file = self.path / filename
+        clean_data = []
+        dict = []
+        dataset_vocab = {}
+        object_id=0
+        with open(str(vg_file), 'r') as vgf:
+            data = json.load(vgf)
             for image in data:
-                image_count += 1
-                if image_count < 5000 :
-                    image_id = image["image_id"]
-                    images_path = path / "images" / "VG_100K"
-                    image_annotation = '{}/{}.jpg'.format(images_path, image_id)
-                    for image_object in image["objects"]:
-                        cur_object_id = 0
-                        x_min = image_object["x"]
-                        y_min = image_object["y"]
-                        x_max = image_object["x"] + image_object["w"]
-                        y_max = image_object["y"] + image_object["h"]
-                        object_names = image_object["names"][0].replace('.', ' ').replace('/', ' ').replace(';', ' ').replace('\'', ' ').replace('\"', ' ').replace(',', ' ').replace('\0', ' ').split(' ')
-                        for object_name in object_names:
-                            if object_name not in object_to_id:
-                                object_to_id[object_name] = object_id
-                                cur_object_id = object_id
-                                object_id += 1
-                            else:
-                                cur_object_id = object_to_id[object_name]
-                            object_string = '{},{},{},{},{}'.format(x_min, y_min, x_max, y_max, cur_object_id)
-                            image_annotation = '{} {}'.format(image_annotation, object_string)
-                    image_annotation = '{}\n'.format(image_annotation)
-                    yf.write(image_annotation)
-    print(len(object_to_id))
-    object_to_id_list = ["" for i in range(0, len(object_to_id))]
-    for key, value in object_to_id.items():
-        object_to_id_list[value] = key
+                updated_image = image
+                updated_image["objects"] = []
+                for image_object in image["objects"]:
+                    updated_image_object = image_object
+                    text = re.sub('[^A-Za-z]+', ' ', image_object["names"][0].lower())
+                    updated_image_object["names"] = text
+                    words = text.split(" ")
+                    for word in words:
+                        if word in dataset_vocab:
+                        #si le mot est déjà dans le vocabulaire, on increment l'occurence
+                            (cur_object_id, occurence) = dataset_vocab[word]
+                            occurence += 1
+                            dataset_vocab[word] = (cur_object_id, occurence)
+                        else:
+                        # si le mot n'est pas dans le vocabulaire
+                            dataset_vocab[word] = (object_id, 1)
+                            cur_object_id = object_id
+                            object_id += 1
+                    updated_image["objects"].append(updated_image_object)
+                clean_data.append(updated_image)
 
-    with open(str(id_to_object_file), 'w+') as itof:
-        for object_name in object_to_id_list:
-            itof.write('{}\n'.format(object_name))
+        print(clean_data)
+    def convert_object_for_yolo_v3(self, filename="objects.json"):
+        vg_file = self.path / filename
+        yolo_file = self.path / "yolo_objects"
+        id_to_object_file = self.path / "yolo_object_to_id"
+        object_to_id = {}
+        object_id=0
+        pairs = 0
+        with open(str(vg_file), 'r') as vgf:
+            with open(str(yolo_file), 'w+') as yf:
+                data = json.load(vgf)
+                image_count = 0
+                for image in data:
+                    image_count += 1
+                    if image_count < 500000 :
+                        image_id = image["image_id"]
+                        images_path = self.path / "images" / "VG_100K"
+                        image_annotation = '{}/{}.jpg'.format(images_path, image_id)
+                        for image_object in image["objects"]:
 
+                            cur_object_id = 0
+                            x_min = image_object["x"]
+                            y_min = image_object["y"]
+                            x_max = image_object["x"] + image_object["w"]
+                            y_max = image_object["y"] + image_object["h"]
+                            #only keep
+                            line = re.sub('[^A-Za-z]+', ' ', image_object["names"][0].lower())
 
-def convert_object_for_retina(path= Path('.'), filename="objects.json"):
-    densecap_file = path / filename
-    yolo_file = path / "retina_objects"
-    id_to_object_file = path / "retina_object_to_id"
-    object_to_id = {}
-    object_id=0
-    with open(str(densecap_file), 'r') as df:
-        with open(str(yolo_file), 'w+') as yf:
-            data = json.load(df)
-            image_count = 0
-            for image in data:
-                image_count += 1
-                if image_count < 5000 :
-                    image_id = image["image_id"]
-                    image_path = path / 'images/VG_100K/{}.jpg'.format(image_id)
-                    for image_object in image["objects"]:
-                        cur_object_id = 0
-                        x_min = image_object["x"]
-                        y_min = image_object["y"]
-                        x_max = image_object["x"] + image_object["w"]
-                        y_max = image_object["y"] + image_object["h"]
-                        if y_min >= y_max:
-                            y_max = y_min + 1
-                        if x_min >= x_max:
-                            x_max = x_min + 1
-                        object_names = image_object["names"][0].replace('.', ' ').replace('/', ' ').replace(';', ' ').replace('\'', ' ').replace('\"', ' ').replace(',', ' ').replace('\0', ' ').split(' ')
-                        for object_name in object_names :
-                            if object_name not in object_to_id:
-                                object_to_id[object_name] = object_id
-                                cur_object_id = object_id
-                                object_id += 1
-                            else:
-                                cur_object_id = object_to_id[object_name]
-                            object_string = '{},{},{},{},{}'.format(x_min, y_min, x_max, y_max, object_name)
-                            image_annotation = '{},{}'.format(image_path, object_string)
-                            image_annotation = '{}\n'.format(image_annotation)
-                            yf.write(image_annotation)
-    print(len(object_to_id))
-    with open(str(id_to_object_file), 'w+') as itof:
+                                object_string = '{},{},{},{},{}'.format(x_min, y_min, x_max, y_max, cur_object_id)
+                                image_annotation = '{} {}'.format(image_annotation, object_string)
+                        image_annotation = '{}\n'.format(image_annotation)
+                        yf.write(image_annotation)
+        print(len(object_to_id))
+        object_to_id_list = ["" for i in range(0, len(object_to_id))]
+        single_words = 0
         for key, value in object_to_id.items():
-            itof.write('{},{}\n'.format(str(key), str(value)))
+            if value[1] < 5:
+                print(key, " : ", value[1])
+                single_words +=1
+            object_to_id_list[value[0]] = key
+        print("Number of few occurent word: ", single_words)
 
+        with open(str(id_to_object_file), 'w+') as itof:
+            for object_name in object_to_id_list:
+                itof.write('{}\n'.format(object_name))
 
-def display_image_bb(path= Path('.'), filename="yolo_objects", id_mapping="yolo_object_to_id"):
-    yolo_file = path / filename
-    id_mapping_file = path / id_mapping
-    id_to_object = []
-    with open(str(id_mapping_file), 'r') as m:
-        for line in m:
-            id_to_object.append(line)
-    #print(id_to_object)
-    with open(str(yolo_file), 'r') as f:
-        data = f.readline()
-        data = data.split()
-        image_file = path / "images" / "VG_100K" / '{}.jpg'.format(data[0])
-        print(image_file)
-        img = cv2.imread( str(image_file), cv2.IMREAD_UNCHANGED)
-        for i in range(1, len(data)):
-            object = data[i].split(',')
-            cv2.rectangle(img, (int(object[0]), int(object[1])), (int(object[2]), int(object[3])), (0, 255, 0), 5)
-            cv2.putText(img, id_to_object[int(object[4])], (int(object[0]), int(object[1]) + 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2, cv2.LINE_AA)
-        cv2.imshow('image', img)
-        cv2.waitKey(0)
-        cv2.destroyAllWindows()
+    def convert_object_for_retina(self, filename="objects.json"):
+        densecap_file = self.path / filename
+        yolo_file = self.path / "retina_objects"
+        id_to_object_file = self.path / "retina_object_to_id"
+        object_to_id = {}
+        object_id=0
+        with open(str(densecap_file), 'r') as df:
+            with open(str(yolo_file), 'w+') as yf:
+                data = json.load(df)
+                image_count = 0
+                for image in data:
+                    image_count += 1
+                    if image_count < 5000 :
+                        image_id = image["image_id"]
+                        image_path = self.path / 'images/VG_100K/{}.jpg'.format(image_id)
+                        for image_object in image["objects"]:
+                            cur_object_id = 0
+                            x_min = image_object["x"]
+                            y_min = image_object["y"]
+                            x_max = image_object["x"] + image_object["w"]
+                            y_max = image_object["y"] + image_object["h"]
+                            if y_min >= y_max:
+                                y_max = y_min + 1
+                            if x_min >= x_max:
+                                x_max = x_min + 1
+                            object_names = image_object["names"][0].replace('.', ' ').replace('/', ' ').replace(';', ' ').replace('\'', ' ').replace('\"', ' ').replace(',', ' ').replace('\0', ' ').split(' ')
+                            for object_name in object_names :
+                                if object_name not in object_to_id:
+                                    object_to_id[object_name] = object_id
+                                    cur_object_id = object_id
+                                    object_id += 1
+                                else:
+                                    cur_object_id = object_to_id[object_name]
+                                object_string = '{},{},{},{},{}'.format(x_min, y_min, x_max, y_max, object_name)
+                                image_annotation = '{},{}'.format(image_path, object_string)
+                                image_annotation = '{}\n'.format(image_annotation)
+                                yf.write(image_annotation)
+        print(len(object_to_id))
+        with open(str(id_to_object_file), 'w+') as itof:
+            for key, value in object_to_id.items():
+                itof.write('{},{}\n'.format(str(key), str(value)))
+
+    def check_for_plural(self, object_name, object_to_id):
+        print("not implemented yet")
+        return False
+
+    def check_for_typo(self, object_name, object_to_id):
+        print("not implemented yet")
+        return False
+
+    def display_image_bb(self, filename="yolo_objects", id_mapping="yolo_object_to_id"):
+        yolo_file = self.path / filename
+        id_mapping_file = self.path / id_mapping
+        id_to_object = []
+        with open(str(id_mapping_file), 'r') as m:
+            for line in m:
+                id_to_object.append(line)
+        #print(id_to_object)
+        with open(str(yolo_file), 'r') as f:
+            data = f.readline()
+            data = data.split()
+            image_file = self.path / "images" / "VG_100K" / '{}.jpg'.format(data[0])
+            print(image_file)
+            img = cv2.imread( str(image_file), cv2.IMREAD_UNCHANGED)
+            for i in range(1, len(data)):
+                object = data[i].split(',')
+                cv2.rectangle(img, (int(object[0]), int(object[1])), (int(object[2]), int(object[3])), (0, 255, 0), 5)
+                cv2.putText(img, id_to_object[int(object[4])], (int(object[0]), int(object[1]) + 10), cv2.FONT_HERSHEY_COMPLEX_SMALL, 1, (255, 255, 255), 2, cv2.LINE_AA)
+            cv2.imshow('image', img)
+            cv2.waitKey(0)
+            cv2.destroyAllWindows()
+
+    def load_glove(self, filename="glove.6B.50d.txt"):
+        vocab = {}
+        full_path = self.path / filename
+        print(full_path)
+        with open(str(full_path), 'r') as f:
+            for line in f:
+                splitted_line = line.replace('\n', '').split(' ')
+                vocab[splitted_line[0]] = splitted_line[1:]
+
+        return vocab
