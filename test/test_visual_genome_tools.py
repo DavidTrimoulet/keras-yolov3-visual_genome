32d50cbb4fc176592c3c9f27677b7bdd25223893
David Trimoulet <davidt@hotmail.fr>
2019-06-18 23:21:03 +0200

    [UPDATE] half datacleanning done and fixing yolo pairs creation

--- test/test_visual_genome_tools.py
+++ test/test_visual_genome_tools.py
@@ -9,26 +9,27 @@ class TestVisualGenomeTools(TestCase):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
         vgt = visual_genome_tools.VisualGenomeTools(path_to_visual_genome_folder=p)
-        vgt.convert_object_for_yolo_v3("objects.json" )
+        vgt.convert_object_for_yolo_v3("clean_objects.json")
 
     def test_convert_object_retina(self):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
         vgt = visual_genome_tools.VisualGenomeTools(path_to_visual_genome_folder=p)
-        visual_genome_tools.convert_object_for_retina(  "objects.json" )
+        visual_genome_tools.convert_object_for_retina("clean_objects.json")
 
     def test_image_converter_sanity_check(self):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
-        vgt = visual_genome_tools.VisualGenomeTools(p)
-        visual_genome_tools.display_image_bb("yolo_objects", "yolo_object_to_id")
+        vgt = visual_genome_tools.VisualGenomeTools(path_to_visual_genome_folder=p)
+        vgt.display_image_bb("yolo_objects", "yolo_object_to_id")
 
     def test_load_glove(self):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
         vgt = visual_genome_tools.VisualGenomeTools(p, load_glove=True)
 
-    def test_clean_visual_genome_data(self:
+    def test_clean_visual_genome_data(self):
         p = Path('.')
         p = p / ".." / ".." / "Visual_Genome"
-        vgt = visual_genome_tools.clean_visual_genome_data(filename="objects.json")
+        vgt = visual_genome_tools.VisualGenomeTools(path_to_visual_genome_folder=p)
+        vgt.clean_visual_genome_data(filename="objects.json")
--- tools/visual_genome_tools.py
+++ tools/visual_genome_tools.py
@@ -20,21 +20,23 @@ class VisualGenomeTools:
 
     def clean_visual_genome_data(self, filename="objects.json"):
         vg_file = self.path / filename
-        vg_clean_file = self.path / filename
+        vg_clean_file = self.path / "clean_objects.json"
         clean_data = []
-        dict = []
         dataset_vocab = {}
         object_id=0
         with open(str(vg_file), 'r') as vgf:
             data = json.load(vgf)
             for image in data:
-                updated_image = image
+                #print(image)
+                updated_image = image.copy()
                 updated_image["objects"] = []
                 for image_object in image["objects"]:
-                    updated_image_object = image_object
+                    #print(image_object)
+                    updated_image_object = image_object.copy()
                     text = re.sub('[^A-Za-z]+', ' ', image_object["names"][0].lower())
-                    updated_image_object["names"] = text
+                    #print(image_object["names"][0])
                     words = text.split(" ")
+                    updated_image_object["names"] = words
                     for word in words:
                         if word in dataset_vocab:
                         #si le mot est déjà dans le vocabulaire, on increment l'occurence
@@ -48,13 +50,35 @@ class VisualGenomeTools:
                             object_id += 1
                     updated_image["objects"].append(updated_image_object)
                 clean_data.append(updated_image)
+            for image in clean_data:
+                #print(image)
+                updated_image = image.copy()
+                updated_image["objects"] = []
+                for image_object in image["objects"]:
+                    #print(image_object)
+                    updated_image_object = image_object.copy()
+                    text = re.sub('[^A-Za-z]+', ' ', image_object["names"][0].lower())
+                    #print(image_object["names"][0])
+                    words = text.split(" ")
+                    isSplitted = False
+                    for word in words:
+                        for vocab_word in dataset_vocab.items():
+                            if vocab_word != word and vocab_word in word :
+                                isSplitted = True
 
-        print(clean_data)
+                            #check if is composed of
+
+                    updated_image_object["names"] = words
+                    updated_image["objects"].append(updated_image_object)
+                clean_data.append(updated_image)
+        with open(str(vg_clean_file), 'w+') as vgf:
+            json.dump(clean_data, vgf)
+
     def convert_object_for_yolo_v3(self, filename="objects.json"):
         vg_file = self.path / filename
         yolo_file = self.path / "yolo_objects"
         id_to_object_file = self.path / "yolo_object_to_id"
-        object_to_id = {}
+        dataset_vocab = {}
         object_id=0
         pairs = 0
         with open(str(vg_file), 'r') as vgf:
@@ -68,23 +92,30 @@ class VisualGenomeTools:
                         images_path = self.path / "images" / "VG_100K"
                         image_annotation = '{}/{}.jpg'.format(images_path, image_id)
                         for image_object in image["objects"]:
-
-                            cur_object_id = 0
                             x_min = image_object["x"]
                             y_min = image_object["y"]
                             x_max = image_object["x"] + image_object["w"]
                             y_max = image_object["y"] + image_object["h"]
-                            #only keep
-                            line = re.sub('[^A-Za-z]+', ' ', image_object["names"][0].lower())
-
+                            words = image_object["names"][0].split(" ")
+                            for word in words:
+                                if word in dataset_vocab:
+                                    # si le mot est déjà dans le vocabulaire, on increment l'occurence
+                                    (cur_object_id, occurence) = dataset_vocab[word]
+                                    occurence += 1
+                                    dataset_vocab[word] = (cur_object_id, occurence)
+                                else:
+                                    # si le mot n'est pas dans le vocabulaire
+                                    dataset_vocab[word] = (object_id, 1)
+                                    cur_object_id = object_id
+                                    object_id += 1
                                 object_string = '{},{},{},{},{}'.format(x_min, y_min, x_max, y_max, cur_object_id)
                                 image_annotation = '{} {}'.format(image_annotation, object_string)
                         image_annotation = '{}\n'.format(image_annotation)
                         yf.write(image_annotation)
-        print(len(object_to_id))
-        object_to_id_list = ["" for i in range(0, len(object_to_id))]
+        print(len(dataset_vocab))
+        object_to_id_list = ["" for i in range(0, len(dataset_vocab))]
         single_words = 0
-        for key, value in object_to_id.items():
+        for key, value in dataset_vocab.items():
             if value[1] < 5:
                 print(key, " : ", value[1])
                 single_words +=1
